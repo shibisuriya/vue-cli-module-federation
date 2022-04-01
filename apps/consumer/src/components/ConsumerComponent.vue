dde82655788bf4c7e0583250186a0523200cb94b
Shibi Suriya <shibisuriya@facilio.com>
2022-04-01 14:46:41 +0530

    Added dummy components

---
+++ apps/consumer/src/components/ConsumerComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="container">
+    <h1>Consumer Component</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#container {
+  border: 1px solid black;
+  text-align: center;
+  color: white;
+  background-color: red;
+  margin: 1em auto;
+  max-width: 15em;
+}
+</style>
---
+++ apps/mfe/src/components/microFrontendComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="container">
+    <h1>Microfrontend Component</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#container {
+  border: 1px solid black;
+  text-align: center;
+  color: white;
+  background-color: black;
+  margin: 1em auto;
+  max-width: 15em;
+}
+</style>
